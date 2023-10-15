91023012e4d15e0cc56090639518a928a36b8a6b
Luis Barbosa <luiselbarbosa@gmail.com>
2023-10-14 22:50:35 -0300

    feat: first commit
    Signed-off-by: Luis Barbosa <luiselbarbosa@gmail.com>

---
+++ src/test/kotlin/io/workshop/tdd/TddApplicationTests.kt
@@ -0,0 +1,19 @@
+package io.workshop.tdd
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+	@Test
+	fun testOne() {
+		assertTrue(true)
+	}
+	
+}
